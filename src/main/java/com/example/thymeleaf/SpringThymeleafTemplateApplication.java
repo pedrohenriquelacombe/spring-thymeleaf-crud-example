df36c98fc9e5e696562db3c1cfa2ca07549fa29d
Pedro Lacombe <pedrocarlantonio@gmail.com>
2021-07-15 03:35:41 -0300

    Removed FeignClient from project

--- src/main/java/com/example/thymeleaf/SpringThymeleafTemplateApplication.java
+++ src/main/java/com/example/thymeleaf/SpringThymeleafTemplateApplication.java
@@ -2,9 +2,7 @@ package com.example.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class SpringThymeleafTemplateApplication {
 
